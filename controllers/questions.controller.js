0051812e752b5f76ac2da7abfd6f727795b9d178
saurabh <saurabhfarkade83@gmail.com>
2023-01-28 15:45:42 +0530

    done

--- controllers/questions.controller.js
+++ controllers/questions.controller.js
@@ -1,8 +1,7 @@
 const { Questionmodel } = require("../models/question.model");
 
 const Quizdatavcontroller = async (req, res) => {
-  const category = req.query.category;
-  const difficulty = req.query.difficulty;
+  const { category, difficulty } = req.query;
   const data = await Questionmodel.find({
     category,
     difficulty,
