5026afcaa80159fa65f544bbeddfdb83380016ba
saurabh <saurabhfarkade83@gmail.com>
2023-01-28 14:32:52 +0530

    done

--- controllers/questions.controller.js
+++ controllers/questions.controller.js
@@ -1,12 +1,12 @@
 const { Questionmodel } = require("../models/question.model");
 
 const Quizdatavcontroller = async (req, res) => {
-  const { category,difficulty } = req.query;
+  const { category, difficulty } = req.query;
   const data = await Questionmodel.find({
-    category: category,
-    difficulty: difficulty,
+    category,
+    difficulty,
   });
-  
+
   res.send(data);
 };
 module.exports = { Quizdatavcontroller };
